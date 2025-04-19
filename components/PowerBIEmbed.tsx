ba3e02af691a43e952b6d28ffa6386d020b8b76f
alfreuq <alfreuq@gmail.com>
2025-04-19 15:39:47 +0200

    act vision dashboards

--- components/PowerBIEmbed.tsx
+++ components/PowerBIEmbed.tsx
@@ -9,7 +9,7 @@ interface PowerBIEmbedProps {
 
 const PowerBIEmbed = ({ url, title }: PowerBIEmbedProps) => {
   return (
-    <div className="w-full aspect-[16/9] min-h-[617px] mb-20"> {/* <- margen inferior extra */}
+    <div className="w-full aspect-[16/9] min-h-[618px] mb-20"> {/* <- margen inferior extra */}
       {title && (
         <h1 className="text-1xl md:text-4xl font-bold mb-6">
           {title}
